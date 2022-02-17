ac51b4182f69e2c057633b4cc79135fff8a59102
sivananda.panda <sivananda.panda1@gmail.com>
2022-02-17 12:20:41 +0800

    restructuring pom.xml

--- rest-heroes/src/main/kotlin/org/playground/superheroes/heroes/repository/HeroRepository.kt
+++ rest-heroes/src/main/kotlin/org/playground/superheroes/heroes/repository/HeroRepository.kt
@@ -7,23 +7,14 @@ import org.playground.superheroes.heroes.model.Hero
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
-
 @ApplicationScoped
 class HeroRepository : PanacheRepository<Hero> {
-
-    /*fun findRandom() =
-        count()
-            .map { if (it > 0) it else null }
-            .onItem().ifNotNull().transform { it?.let { it1 -> Random().nextInt(it1.toInt()) } }
-            .onItem().ifNotNull()
-            .transformToUni { it?.let { it1 -> findAll().page<Hero>(Page.of(it1, 1)).firstResult<Hero>() } }*/
 
     fun findRandom(): Uni<Hero>? {
         return count()
             .map { count: Long -> if (count > 0) count else null }
             .onItem().ifNotNull().transform { count: Long? -> Random().nextInt(count!!.toInt()) }
-            .onItem().ifNotNull().transformToUni { randomHero ->
-                findAll().page<Hero>( Page.of(randomHero, 1)).firstResult()
-            }
+            .onItem().ifNotNull().transformToUni { randomHero -> findAll().page<Hero>( Page.of(randomHero, 1)).firstResult() }
     }
+
 }
